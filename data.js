63b63be91bb6b139c590b1c00508d60d87a824a2
Sorrawit Eaknarongpan <614259055@webmail.npru.ac.th>
2020-09-28 11:25:14 +0700

    add foodgrab in github

---
+++ data.js
@@ -0,0 +1,65 @@
+const restaurants=[
+{
+    id : 1,
+    name : "McDonald's (แมคโดนัลด์) - พาต้า ปิ่นเกล้า",
+    type : "ร้านอาหาร",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000004bl/hero/5c2f639864be46968813496061ab209c_1600008272596123546.jpeg"
+},
+{
+    id : 2,
+    name : "Cafe Amazon (คาเฟ่ อเมซอน) - อาคารพาณิชย์ปิ่นเกล้า",
+    type : "ร้านกาแฟ",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZKKA2XXFGABCX/hero/1769c1934ab141c1b78c574304d2610e_1593409065311657054.jpeg"
+},
+{
+    id : 3,
+    name : "นายอ้วนเย็นตาโฟเสาชิงช้า - ถนนตะนาว",
+    type : "ร้านอาหารเส้น",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000022y/hero/c4b9c9ca49f84274861dc367529d3cc0_1592216282560213295.jpeg"
+},
+{
+    id : 4,
+    name : "ราดหน้าเฮงยอดผัก - เยาวราช",
+    type : "ร้านอาหารเส้น",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST0000002d/hero/dca01259d4e540fcbf6c7e398312737f_1589724061440001768.jpeg"
+},  
+ {
+    id : 5,
+    name : "Passion Food อาหารคลีนเดลิเวอรี่ - สุขุมวิท",
+    type : "ร้านอาหารสุขภาพ",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZCEL7JKPAWGTT/hero/4e69a04f39db436b99498b5929cf0984_1596162680320444445.jpeg"
+},    
+{
+    id : 6,
+    name : "ข้าวมันไก่ป้าจ๋า 23 ชั่วโมง - ถนนจันทรคามพิทักษ์",
+    type : "ร้านอาหารตามสั่ง",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZE1VBLYNJMJNN/hero/3a6cdd30d6594ffaabe2b2ac7eee93a1_1589790546369189511.jpeg"
+},   
+ {
+    id : 7,
+    name : "Kamu Tea (คามุ ที) - ถนนจันทรคามพิทักษ์",
+    type : "ร้านเครื่องดื่ม",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZABBF3KDA3YAT/hero/cd21c61c294c45eaae957d631f4f3bdd_1580109720841697666.jpeg"
+},    
+{
+    id : 8,
+    name : "พอดีคำ เมี่ยงหมิวสลัดโรล - ถนนทรงพล",
+    type : "ร้านอาหารสุขภาพ",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZNDLNJEG6MZJ6/hero/1ad1d01ec62b42d28e6b80a1873235a2_1598680317338129190.jpeg"
+},  
+{
+    id : 9,
+    name : "Toba Cafe - ลำพยา",
+    type : "อาหารตามสั่ง",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZJ1RXKEEVLKJN/hero/0b1bf7d2ce6044c3a23e4fd3ccd4669d_1600006273618721955.jpeg"
+},  
+ 
+{
+    id : 10,
+    name : "วอฟเฟิลฮ่องกงmrplan - ถนนเพชรเกษม",
+    type : "ขนมขบเคี้ยว",
+    imageURL : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZDHREVVCCNBGA/hero/112058f4de0b46089a28c8a60ccf99c1_1588662677388124709.jpeg"
+}
+]
+
+module.exports = restaurants;
